244667f8f5245a045d181c0947ca3063440e23c0
hogumachu <castle_jun@daum.net>
2026-01-02 00:58:07 +0900

    Update home page ui

--- Sources/Pages/HomePage.swift
+++ Sources/Pages/HomePage.swift
@@ -6,18 +6,18 @@ struct HomePage: StaticPage {
   
   var body: some HTML {
     VStack(spacing: .xLarge) {
-      Spacer()
+      Spacer(size: 100)
       
       Text("THE<br>STEADY<br>COMPANY")
         .font(.custom("Space Grotesk", size: .em(2), weight: .bold))
         .horizontalAlignment(.center)
       
-      VStack(spacing: .medium) {
-        content("🪴 Garden", target: GardenPage().path)
-        content("📰 Personal Blog", target: Links.personalBlog.target)
-      }
+      Spacer(size: 100)
       
-      SocialFooterComponent()
+      Grid {
+        content("🪴", target: GardenPage().path)
+        content("📰", target: Links.personalBlog.target)
+      }
       
       Spacer()
     }
@@ -26,14 +26,19 @@ struct HomePage: StaticPage {
   @HTMLBuilder
   private func content(_ title: String, target: String) -> some HTML {
     LinkGroup(target: target) {
-      HStack(spacing: .medium) {
+      VStack(spacing: .medium) {
         Text(title)
           .foregroundStyle(.primary)
+          .font(.title1)
         
         Image(systemName: "arrow-right-circle-fill", description: "arrow")
           .resizable()
+          .font(.title4)
       }
-      .font(.title4)
+      .padding(.vertical, .large)
+      .padding(.horizontal, .xLarge)
+      .background(.gray.opacity(0.05))
+      .cornerRadius(16)
     }
     .hoverTransition()
   }
